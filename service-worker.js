de779cf7c2cf30b464187774628c51cc4ba4f267
caiazzoalfonso97-web <caiazzoalfonso97@gmail.com>
2026-02-25 18:37:38 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'book-log-v1';
+const FILES = [
+  './book-log.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
